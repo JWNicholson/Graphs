22b4eea0f9c6a7f3f22661b8870cc7e67623d180
JWNicholson <john.nicholson812@gmail.com>
2020-08-13 20:30:05 -0400

    refactored and cleanedup code.MVP reached

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -30,28 +30,80 @@ player = Player(world.starting_room)
 traversal_path = []
 
 # set direction possibles
-
+facing_directions = {"e": "w", "w":"e", "n": "s", "s": "n"}
 # set Player starting room
-
+starting_room = player.current_room
 # dictionary to track visited rooms
-
+visited = {}
 # list for storing reverse directions
-
-# Find un-explored direct
-
+reversed_direction = []
 # set initial graph with starting room
+current_room = starting_room
 
-# Decide the next direction
+# Find un-explored direction
+def move_quest(current_room):
+		curr_room = current_room.id
+		room_exits = visited[curr_room]
 
-# Check if at a dead-end. If yes, look for exits = ?
+    # Check all coordinates and return a direction not yet evaluated
+		for direction in room_exits:
+			if room_exits[direction] == '?' and current_room.get_room_in_direction(direction).id not in visited:
+				return direction
+		return None
+# Check for unexplored rooms exits
+def search_unexplored_exits():
+    while True:
+        back_direction = reversed_direction.pop()
+        traversal_path.append(back_direction)
+        player.travel(back_direction)
+        next_room = player.current_room
 
-# look for valid exits by backtracking
+     # Find any "?" exits in room. If found, return it
+        if '?' in visited[next_room.id].values():
+            return next_room
 
-# Add direction to the path and move into the next room
+#set up initial traversl graph
+if current_room.id not in visited:
+    curr_exits = current_room.get_exits()
 
-# Track reverse direction so we can backtrack if needed
+    #track un visited exits with a dict
+    exits ={}
+    for xt in curr_exits:
+        exits[xt] ="?"
+        visited[current_room.id] = exits
 
-# Update visted dict
+while len(visited) < len(world.rooms):
+    # next direction
+    next_direction = move_quest(current_room)
+    # Check if at a dead-end. If yes, look for exits = ?
+    if next_direction is None:
+        # look for valid exits by backtracking
+        new_room = search_unexplored_exits()
+        current_room = new_room
+
+    else:
+        # Add direction to the path and move into the next room
+        player.travel(next_direction)
+        traversal_path.append(next_direction)
+
+        # Track reverse direction so we can backtrack if needed
+        reversed_direction.append(facing_directions[next_direction])
+        previous_room = current_room
+        # set current room to the new room
+        current_room = player.current_room
+
+    # Update visted dict
+    new_exits = {}
+    for xt in current_room.get_exits():
+        new_exits[xt] = "?"
+        visited[current_room.id] = new_exits
+
+        #set previous direction to the new room
+        visited[previous_room.id][next_direction] = current_room.id
+
+        # set the new room direction
+        visited[previous_room.id][next_direction] = current_room.id
+       
 
 # TRAVERSAL TEST
 visited_rooms = set()
